025c49056e82ff5558b562fb005f342935465e23
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-03-10 16:50:57 -0300

    Ribbon load balacing

--- hr-payrool/src/main/java/com/example/hrpayroll/HrPayroolApplication.java
+++ hr-payrool/src/main/java/com/example/hrpayroll/HrPayroolApplication.java
@@ -2,8 +2,13 @@ package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+/*
+ * Agora utilizando o ribbon client, vamos ter que colocar
+ * essa anotation
+ */
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayroolApplication {
--- hr-payrool/src/main/java/com/example/hrpayroll/WorkerFeignClient.java
+++ hr-payrool/src/main/java/com/example/hrpayroll/WorkerFeignClient.java
@@ -8,15 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.hrpayroll.entities.Worker;
 
-
-
 /*
  * O FeignClient serve para conectar os microsservicos de hrpayroll com o hrworker.
  * A partir daqui a conexão entre os serviços serão automáticas, por enquanto
  * é necessário que colocar todos os parametros que estao no resouces->application.proprieties
  */
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path="/workers")
+//@FeignClient(name = "hr-worker", url = "localhost:8001", path="/workers")
+@FeignClient(name = "hr-worker", path="/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value= "/{id}")
