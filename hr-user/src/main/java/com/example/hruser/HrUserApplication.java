728326a60110ec6307ae4ea8a8bbdbcc955ebd45
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-03-17 16:38:01 -0300

    Carga Inicial de Banco de Dados (seeding)

--- hr-user/src/main/java/com/example/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/example/hruser/HrUserApplication.java
@@ -1,15 +1,32 @@
 package com.example.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*
+ * O ComendLineRunner é para implementar um codificador
+ * de senhas que aqui é chamado de BCrypt.
+ */
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Gerando um possível bcrypt para a senha 123456, já que usa o hash que pode mudar com a mesma String
+		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
 	}
 
 }
